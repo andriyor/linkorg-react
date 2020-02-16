5720c2c0d1af1a443c5b2196373cba3f24175130
Andriy Orehov <andriyorehov@gmail.com>
2020-02-16 20:49:17 +0200

    test: e2e: add telegram test

--- src/App.test.js
+++ src/App.test.js
@@ -27,4 +27,30 @@ describe('posts', () => {
 
     browser.close();
   }, 16000);
+  test('telegram', async () => {
+    let browser = await puppeteer.launch({headless: false});
+    let page = await browser.newPage();
+
+    const NEW_URL= 'https://t.me/libmustdie/4025';
+    const EXPECTED_IFRAME_URL= 'https://t.me/libmustdie/4025?embed=1';
+
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('[data-test-id="post-url"]');
+    await page.click('[data-test-id="post-url"]');
+    await page.keyboard.type(NEW_URL);
+
+    await page.click("#data-test-select-id");
+
+    await page.waitForSelector("#react-select-2-option-0");
+    await page.click("#react-select-2-option-0");
+
+    await page.click('[data-test-id="add-post"]');
+
+    const LAST_IFRAME_SELECTOR = '[data-test-id="post-wrapper"]:last-of-type  div iframe';
+    await page.waitFor(1000);
+    const inputValue = await page.$eval(LAST_IFRAME_SELECTOR, e => e.getAttribute('src'));
+    expect(inputValue).toBe(EXPECTED_IFRAME_URL);
+
+    browser.close();
+  }, 16000);
 });
